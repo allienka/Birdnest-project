85be3182256b04eb2155c52af7585c74ff751f47
Alena Hakkarainen <alena.rebova@gmail.com>
2023-01-10 17:16:19 +0200

    added function to close db

--- connection.php
+++ connection.php
@@ -1,15 +1,23 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "drones";
+
+function create_dbc(){
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "drones";
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
 // Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-#echo "Connected";
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    
+    return $conn;
+    
 
-?>
+
+}
+function close_db($conn){
+    return mysqli_close($conn);
+}   
